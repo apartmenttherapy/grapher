37cabf811acc93c877c2dcb2db6a89dad973a686
Glen Holcomb <MentalLabor@gmail.com>
2018-08-27 14:16:58 -0600

    cleanup

--- mix.exs
+++ mix.exs
@@ -28,7 +28,7 @@ defmodule Grapher.Mixfile do
   defp coveralls_config do
     [
       test_coverage: [tool: ExCoveralls],
-      preferred_cli_env: ["coveralls": :test, "coveralls.detail": :test, "coveralls.post": :test, "coveralls.html": :test]
+      preferred_cli_env: [coveralls: :test, "coveralls.detail": :test, "coveralls.post": :test, "coveralls.html": :test]
     ]
   end
 
